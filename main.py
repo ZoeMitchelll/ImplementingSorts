ebebfba3f5f5fa04fd885825270d127115b851af
Zoe Mitchell <mitchellzh101@gmail.com>
2021-02-11 19:09:20 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+
+
+def generate_array(n):
+    array = []
+    random.seed()
+    for i in range(n):
+        random.randint(i,n-)
