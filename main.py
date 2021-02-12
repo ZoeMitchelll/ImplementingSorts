eb0c985fde1e69c00eefcc2d538ae157d57a4301
Zoe Mitchell <mitchellzh101@gmail.com>
2021-02-11 19:49:10 -0600

    Added HayesSort
    A sort my professor wanted me to code

--- main.py
+++ main.py
@@ -10,10 +10,24 @@ def generate_array(n):
     new_num = 1000000
     random.seed()
     while len(array) != n:
-        new_num = random.randint(new_num-20, new_num)
+        new_num = random.randint(new_num - 20, new_num)
         if new_num not in array:
             array.append(new_num)
     print(array)
+
+
+def hayes_sort(array_a):
+    max_val = float('-inf')
+    index = 0
+    for i in range(len(array_a)):
+        if array_a[i] > max_val:
+            max_val = array_a[i]
+    array_b = []
+    for j in range(max_val):
+        if j in array_a:
+            array_b[index] = j
+            index = index + 1
+    return array_b
 
 
 generate_array(50000)
